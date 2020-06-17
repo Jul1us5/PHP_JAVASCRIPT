df8b2a02fa959ae4082fb703f6db269b8e86e4cc
Julius <Julius>
2020-06-18 00:07:33 +0300

    Calc

---
+++ calc.php
@@ -0,0 +1,17 @@
+<form action="" method="get">
+    <input type="text" name="x" id="x" value="<?= $_GET['x'] ?? '' ?>">
+    <input type="text" name="y" id="y" value="<?= $_GET['y'] ?? '' ?>">
+    <input type="submit" value="+" name="action">
+    <input type="submit" value="-" name="action">
+</form>
+<?php
+if (isset($_GET['action']) && isset($_GET['x']) && isset($_GET['y'])) {
+    if ($_GET['action'] == '+') {
+        echo '<h1>' . ((int) $_GET['x'] + (int) $_GET['y']) . '</h1>';
+    } elseif ($_GET['action'] == '-') {
+        echo '<h1>' . ((int) $_GET['x'] - (int) $_GET['y']) . '</h1>';
+    }
+    // echo json_encode($_GET);
+} else {
+    echo 'iveskite skaiciukus';
+}
