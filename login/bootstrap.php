a88d3586d38f5bba6eec2b65ec301ff7552346da
Julius <Julius>
2020-06-22 21:00:14 +0300

    constructor

--- login/bootstrap.php
+++ login/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$data = [
-        ['name' => 'Julius', 'pass' => md5('123')],
-        ['name' => 'AloYzas', 'pass' => md5('123')],
-];
+// $data = [
+//         ['name' => 'Julius', 'pass' => md5('123')],
+//         ['name' => 'AloYzas', 'pass' => md5('123')],
+// ];
 
-// $data = json_decode(file_get_contents(__DIR__ .'/data.json'),1);
+$data = json_decode(file_get_contents(__DIR__ .'/data.json'),1);
 
